0306f5cd6e964505ad8dcb305c904853b61a7ea5
SKKCN <shukuan2717@163.com>
2018-08-11 18:33:18 +0800

    Desktop哈哈?
    Desktop哈哈?

--- src/com/itheima/GitTest.java
+++ src/com/itheima/GitTest.java
@@ -2,6 +2,6 @@ package com.itheima;
 
 public class GitTest {
     public static void main(String[] args) {
-        System.out.println("HelloGit");
+        System.out.println("HelloGit?!");
     }
 }
