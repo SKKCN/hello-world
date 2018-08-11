dda3a4887ed905c36b9d30204a2d399fe63f1039
SKKCN <shukuan2717@163.com>
2018-08-11 18:10:52 +0800

    哈哈

--- src/com/itheima/com/itheima/test/ThisIsATest.java
+++ src/com/itheima/com/itheima/test/ThisIsATest.java
@@ -2,6 +2,6 @@ package com.itheima.com.itheima.test;
 
 public class ThisIsATest {
     public static void main(String[] args) {
-        System.out.println("it's a fucking test!");
+        System.out.println("it's a fucking test!U See????");
     }
 }
